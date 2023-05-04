d9742f181d46da273b0c54676f500338e7448502
Abner Alain  Gil Garcia <124418451+AbnerAlg@users.noreply.github.com>
2023-05-04 11:02:28 -0600

    Se sube archivo

--- Locales/src/main/java/com/mycompany/locales/Locales.java
+++ Locales/src/main/java/com/mycompany/locales/Locales.java
@@ -8,9 +8,23 @@ package com.mycompany.locales;
  *
  * @author abner
  */
-public class Locales {
+public class CalcularAreaTriangulo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Llamamos al método que realiza el cálculo del área del triángulo
+        calcularAreaTriangulo();
+    }
+
+    public static void calcularAreaTriangulo() {
+        // Definimos las variables necesarias para el cálculo del área
+        double base = 10.0;
+        double altura = 5.0;
+        double area;
+
+        // Calculamos el área del triángulo
+        area = (base * altura) / 2;
+
+        // Imprimimos el resultado por consola
+        System.out.println("El área del triángulo es: " + area);
     }
 }
